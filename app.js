88482853ceb8a5f749d1bdb1a65d3c837d73d4f5
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-19 15:14:11 -0600

    Basic get route created and working

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
+0
+app.get("/", (request, response) => {
+    response.send('THE ROUTE WORKED!')
+})
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
